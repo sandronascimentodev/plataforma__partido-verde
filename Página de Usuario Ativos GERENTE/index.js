02c087fafa4af539fad0de6d8a23739fe84c90ef
Sandro Junior Lourenço Nascimento <sandro.nascimento@portovelho.intranet.br>
2024-03-28 11:31:27 -0400

    Implementando Lista de Usuário Ativos Clicáveis
    Implementação de função javascript para deixar o elemento TR do html clicavel.

--- Página de Usuario Ativos GERENTE/index.js
+++ Página de Usuario Ativos GERENTE/index.js
@@ -2,6 +2,19 @@ const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const themeToggler = document.querySelector(".theme-toggler");
+const clicavel = document.querySelector("#clicavel");
+const url = '/Página Principal GERENTE/index.html';
+
+//função para chamar a url e abrir uma nova página
+function openInNewTab(url) {
+    const win = window.open('/Página Principal GERENTE/index.html', '_blank')
+    win.focus()
+}
+
+//clicar no usuário ativo e abrir uma nova página
+clicavel.addEventListener('click', () => {
+    openInNewTab(url)
+})
 
 //show sidebar
 menuBtn.addEventListener('click', () => {
@@ -22,7 +35,7 @@ themeToggler.addEventListener('click', () => {
 })
 
 // fill orders in table
-Orders.forEach(order => {
+/*Orders.forEach(order => {
     const tr = document.createElement('tr');
     const trContent = `
                     <td>${order.numero}</td>
@@ -31,9 +44,10 @@ Orders.forEach(order => {
                     <td>${order.contatosCadastrados}</td>
                     <td>${order.ultimoAcesso}</td>
                     <td class="primary">...</td>
+        
                     `;
 
     tr.innerHTML = trContent;
     document.querySelector('table tbody').appendChild(tr);
 
-})
+})*/
